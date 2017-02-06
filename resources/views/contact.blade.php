6fd048f5741c5c32b3356ab8d8e4d9afb26b6308
Waqar Mohammad <waqar@waqar.co>
2017-02-06 14:56:14 +0000

    Day One
    Installed Laravel and created first project with some extra views

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -79,18 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome Everyone
+                    Contact me
                 </div>
                 <div>
-                    <p>This is the beginning of your Laravel Journey</p>
+                    <p>I would love to chat! Email me at waqar@waqar.co</p>
                 </div>
 
                 <div class="links">
-                    <!--<a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>-->
                 </div>
             </div>
         </div>
