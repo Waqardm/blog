a502a22cf0f146c4a389429724979a8286bdd756
Waqar Mohammad <waqar@waqar.co>
2017-02-08 10:11:06 +0000

    Day 3
    - Blade sections & partials
    - Models
    - Creating tables in Laravel
    - Migrations

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,98 +1,51 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Welcome Everyone
-                </div>
-                <div>
-                    <p>This is the beginning of my Laravel Blog</p>
-                </div>
-
-                <div class="links">
-                    <!--<a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>-->
+@extends('main')
+@section('title', 'Home')
+@section('content')
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron">
+                  <h1>Welcome to My Blog!</h1>
+                  <p class="lead">Thanks you so much for visiting. This is my test website built with Laravel. Please read the popular post!</p>
+                  <p><a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a></p>
                 </div>
             </div>
+        </div> <!-- end of header .row -->
+
+        <div class="row">
+          <div class="col-md-8">
+              <div class="post">
+                  <h3>Post Title</h3>
+                  <p>asjakjsndkasjnkajsnkajsd kjansdkjnasdkjansdkjnasdkjnasdkjn...</p>
+                  <a href="#" class="btn btn-primary">Read More</a>
+              </div>
+              
+              <hr>
+
+              <div class="post">
+                  <h3>Post Title</h3>
+                  <p>asjakjsndkasjnkajsnkajsd kjansdkjnasdkjansdkjnasdkjnasdkjn...</p>
+                  <a href="#" class="btn btn-primary">Read More</a>
+              </div>
+
+              <hr>
+
+              <div class="post">
+                  <h3>Post Title</h3>
+                  <p>asjakjsndkasjnkajsnkajsd kjansdkjnasdkjansdkjnasdkjnasdkjn...</p>
+                  <a href="#" class="btn btn-primary">Read More</a>
+              </div>
+
+              <hr>
+
+              <div class="post">
+                  <h3>Post Title</h3>
+                  <p>asjakjsndkasjnkajsnkajsd kjansdkjnasdkjansdkjnasdkjnasdkjn...</p>
+                  <a href="#" class="btn btn-primary">Read More</a>
+              </div>
+          </div>
+
+          <div class="col-md-3 col-md-offset-1">
+              <h2>Sidebar</h2>
+          </div>
         </div>
-    </body>
-</html>
+@endsection
