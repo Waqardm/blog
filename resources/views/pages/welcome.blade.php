da85c18df8e15a50b7c53215e15d9dfd2d950c3b
Waqar Mohammad <waqar@waqar.co>
2017-02-10 21:11:47 +0000

    -all

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -13,35 +13,19 @@
 
         <div class="row">
           <div class="col-md-8">
-              <div class="post">
-                  <h3>Post Title</h3>
-                  <p>asjakjsndkasjnkajsnkajsd kjansdkjnasdkjansdkjnasdkjnasdkjn...</p>
-                  <a href="#" class="btn btn-primary">Read More</a>
-              </div>
+
+              @foreach($posts as $post)
+
+                <div class="post">
+                    <h3>{{ $post->title }}</h3>
+                    <p>{{ substr($post->body, 0, 300) }}{{ strlen($post->body) > 300 ? "...": "" }}</p>
+                    <a href="" class="btn btn-primary">Read More</a>
+                </div>
+                
+                <hr>
               
-              <hr>
+              @endforeach
 
-              <div class="post">
-                  <h3>Post Title</h3>
-                  <p>asjakjsndkasjnkajsnkajsd kjansdkjnasdkjansdkjnasdkjnasdkjn...</p>
-                  <a href="#" class="btn btn-primary">Read More</a>
-              </div>
-
-              <hr>
-
-              <div class="post">
-                  <h3>Post Title</h3>
-                  <p>asjakjsndkasjnkajsnkajsd kjansdkjnasdkjansdkjnasdkjnasdkjn...</p>
-                  <a href="#" class="btn btn-primary">Read More</a>
-              </div>
-
-              <hr>
-
-              <div class="post">
-                  <h3>Post Title</h3>
-                  <p>asjakjsndkasjnkajsnkajsd kjansdkjnasdkjansdkjnasdkjnasdkjn...</p>
-                  <a href="#" class="btn btn-primary">Read More</a>
-              </div>
           </div>
 
           <div class="col-md-3 col-md-offset-1">
